7473d99aface623d6811180ea24031b6b808f9a2
mariohieu09 <trantrunghieu20092001@gmail.com>
2023-08-16 09:22:20 +0700

    home 20%

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,9 @@
 import "./home.css"
 import {Header} from "../component/header";
+import {Carousel} from '@trendyol-js/react-carousel';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import GradeIcon from '@mui/icons-material/Grade';
 export const Home = () =>{
 
 
@@ -25,10 +29,94 @@ export const Home = () =>{
                             </div>
                         </div>
                         <div id={'release-calender'}>
-                            <span className={'release-time'}>[12:00]</span><span className={'release-name'}>OK con te te</span><br/>
-                            <span className={'release-time'}>[13:33]</span><br/>
-                            <span className={'release-time'}>[13:33]</span><br/>
-                            <span className={'release-time'}>[13:33]</span><br/>
+
+                        </div>
+                        <div id={'cool-stuffs'}>
+                            <span id={'cool-h3'}>Truyện Hay</span>
+                            <Carousel
+                                show={6}
+                                useArrowKeys={false}
+                                infinite={true}
+                                swiping={true}
+                                // autoSwipe={6}
+                                transition={1.5}
+                                leftArrow={<ArrowBackIosNewIcon className={'arrows'}></ArrowBackIosNewIcon>}
+                                rightArrow={<ArrowForwardIosIcon className={'arrows'}></ArrowForwardIosIcon>}
+                                className={'carousel-cool'}
+                            >
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                            <h4>One Piece</h4>
+                                            <span>One Piece</span>
+                                    </div>
+                                </div>
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                        <h4>One Piece</h4>
+                                        <span>One Piece</span>
+                                    </div>
+                                </div>
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                        <h4>One Piece</h4>
+                                        <span>One Piece</span>
+                                    </div>
+                                </div>
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                        <h4>One Piece</h4>
+                                        <span>One Piece</span>
+                                    </div>
+                                </div>
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                        <h4>One Piece</h4>
+                                        <span>One Piece</span>
+                                    </div>
+                                </div>
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                        <h4>One Piece</h4>
+                                        <span>One Piece</span>
+                                    </div>
+                                </div>
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                        <h4>One Piece</h4>
+                                        <span>One Piece</span>
+                                    </div>
+                                </div>
+                                <div className={'comics'}>
+                                    <div className={'comics-picture'}>
+                                        <img src="/op1.webp" alt=""/>
+                                    </div>
+                                    <div className={'comics-name'}>
+                                        <h4>One Piece</h4>
+                                        <span>One Piece</span>
+                                    </div>
+                                </div>
+                            </Carousel>
                         </div>
                     </div>
                 </div>
